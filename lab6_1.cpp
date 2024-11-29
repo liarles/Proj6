c82400dca6f75001d7ecd41c36c767d71ef86167
liarles <pianlemon@gmail.com>
2024-11-29 15:25:06 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main(){
+    int integer;
     cout << "Enter an integer: ";
+    cin >> integer;
     cout << "#Even numbers = ";
+    cin >>
     cout << "#Odd numbers = ";
+    cin >>
     return 0;
 }
+git 
