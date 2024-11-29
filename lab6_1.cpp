333bd315040c4865bbc105a924622c5d7957d26a
liarles <pianlemon@gmail.com>
2024-11-29 15:53:56 +0700

    fine

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main(){
     int i=1;
-    int integer[100];
+    int integer[0];
     while (integer !=0){
     cout << "Enter an integer: ";
     cin >> integer[i];
     i++;
     }
     int j=1;
+    int even[];
     if (integer[j]%2==0){
     cout << "#Even numbers = ";
     cin >> integer [j];
