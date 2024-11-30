c7da1e096b4ec83e8c628a59df49dd8c061737c5
liarles <62631264+liarles@users.noreply.github.com>
2024-11-30 07:04:22 +0700

    lab6_1.cpp
    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,24 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int i=1;
-    int integer[0];
-    while (integer !=0){
-    cout << "Enter an integer: ";
-    cin >> integer[i];
-    i++;
-    }
-    int j=1;
-    int even[];
-    if (integer[j]%2==0){
-    cout << "#Even numbers = ";
-    cin >> integer [j];
-    } 
-    else {
-    cout << "#Odd numbers = ";
-    cin >> integer [j];
+int main() {
+    int num;
+    int evenCount = 0, oddCount = 0;
 
+    while (true) {
+        cout << "Enter an integer: ";
+        cin >> num;
+
+        if (num == 0) {
+            break;
+        }
+
+        if (num % 2 == 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
     }
+
+    cout << "#Even numbers = " << evenCount << endl;
+    cout << "#Odd numbers = " << oddCount << endl;
+
     return 0;
-}
